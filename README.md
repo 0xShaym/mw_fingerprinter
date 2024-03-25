87b37fc97ff92b3b2e6a2627d0faba81b2a43021
PPF-Shaym <matdupont9@gmail.com>
2024-03-25 01:00:27 +0100

    add func

--- README.md
+++ README.md
@@ -1,11 +1,85 @@
 # mw_fingerprinter
 
+## Take fingerprint of your malware
+Malware samples fingerprinter tool. 
+
+### Outputs
+Hashes : 
+- MD5
+- SHA-1
+- SHA-256
+- SSDEEP
+- IMPHASH (for PE samples only)
+
+Command :
+- strings
+- hex dump
+- file
+- trid
+- clamscan
+
+
+## Installation : 
+
 Create venv python
-
+```
 python3 -m venv env
-
+source env/bin/activate
+```
 Install dependecies
+```
 sudo apt install libfuzzy-dev
 sudo apt install libfuzzy2
 sudo apt install ssdeep
+```
+Python modules
+```
 pip install -r requirements.txt
+```
+Clamav
+```
+sudo apt install clamav-freshclam
+sudo apt install clamav
+```
+TrID
+```
+wget http://mark0.net/download/trid_linux_64.zip
+unzip trid_linux_64.zip
+wget http://mark0.net/download/triddefs.zip
+unzip triddefs.zip
+sudo mv trid triddefs.trd /usr/local/bin/
+rm triddefs.zip trid_linux_64.zip readme.txt
+sudo chmod +x /usr/local/bin/trid
+```
+
+## How to use : 
+Pass the path to a folder or path to a sample as an argument :
+```
+$ python3 fingerprinter.py
+usage: fingerprinter.py [-h] [--clamav] sample
+```
+You can specify if you want a clamav scan during the analysis
+## Result :
+```
+$ tree sample/
+sample/
+├── sample0
+├── sample1
+├── sample10
+├── sample2
+├── sample3
+├── sample6
+├── sample7
+├── sample8
+├── sample9
+└── sample_analysed
+    ├── sample0.md
+    ├── sample10.md
+    ├── sample1.md
+    ├── sample2.md
+    ├── sample3.md
+    ├── sample6.md
+    ├── sample7.md
+    ├── sample8.md
+    └── sample9.md
+```
--- fingerprinter.py
+++ fingerprinter.py
@@ -1,8 +1,9 @@
 import hashlib
 import ssdeep
-import pefile  # Assurez-vous d'installer pefile avec pip install pefile
+import pefile
 import argparse
 import os
+import subprocess
 
 def is_pefile(file_path):
     try:
@@ -19,17 +20,14 @@ def calculate_hashes(file_path):
     imphash = ""
 
     with open(file_path, "rb") as file:
-        # Calculer les hachages MD5, SHA-1 et SHA-256
         while chunk := file.read(8192):
             md5_hash.update(chunk)
             sha1_hash.update(chunk)
             sha256_hash.update(chunk)
 
-        # Calculer le hash SSDEEP
         file.seek(0)
         ssdeep_hash = ssdeep.hash(file.read())
 
-        # Calculer le hash IMPHASH pour les fichiers PE
         if is_pefile(file_path):
             pe = pefile.PE(file_path)
             imphash = pe.get_imphash()
@@ -38,46 +36,84 @@ def calculate_hashes(file_path):
 
     return md5_hash.hexdigest(), sha1_hash.hexdigest(), sha256_hash.hexdigest(), ssdeep_hash, imphash
 
-def generate_md_content(file_path, md5, sha1, sha256, ssdeep_hash, imphash):
+def generate_md_content(file_path, md5, sha1, sha256, ssdeep_hash, imphash, clamav_result=None, trid_result=None, file_info=None, strings_output=None, hexdump_head=None, hexdump_tail=None):
     content = f"# File hashes for : {file_path}\n"
-    content += f"MD5: {md5}\n"
-    content += f"SHA-1: {sha1}\n"
-    content += f"SHA-256: {sha256}\n"
-    content += f"SSDEEP: {ssdeep_hash}\n"
-    content += f"IMPHASH: {imphash}\n\n"
+    content += f"MD5 : {md5}\n"
+    content += f"SHA-1 : {sha1}\n"
+    content += f"SHA-256 : {sha256}\n"
+    content += f"SSDEEP : {ssdeep_hash}\n"
+    content += f"IMPHASH : {imphash}\n"
+    content += f"# File Info : \n{file_info}\n"
+    content += f"# ClamAV Result : \n{clamav_result}\n"
+    content += f"# TRID Result : \n{trid_result}\n"
+    content += f"# Hexdump Head : \n{hexdump_head}\n"
+    content += f"# Hexdump Tail : \n{hexdump_tail}\n"
+    content += f"# Strings Output : \n{strings_output}\n\n"
+    
     return content
 
-def analyze_sample(sample_path, output_folder):
+def analyze_sample(sample_path, output_folder, run_clamav=False):
     if not os.path.exists(sample_path):
-        print(f"Le chemin spécifié {sample_path} n'existe pas.")
+        print(f"Path {sample_path} does not exist.")
         return
 
     if os.path.isfile(sample_path):
         md5, sha1, sha256, ssdeep_hash, imphash = calculate_hashes(sample_path)
-        md_content = generate_md_content(sample_path, md5, sha1, sha256, ssdeep_hash, imphash)
+        
+        clamav_result = ""
+        if run_clamav:
+            try:
+                clamav_result = subprocess.run(["clamscan", "--no-summary", sample_path], capture_output=True, text=True).stdout.strip()
+            except Exception as e:
+                print("Try clamscan but an error occurs : ", e)
+        try:
+            trid_result = subprocess.run(["trid", sample_path], capture_output=True, text=True).stdout.strip()
+        except Exception as e:
+            print("Try trid but an error occurs : ", e)
+        try:
+            file_info = subprocess.run(["file", sample_path], capture_output=True, text=True).stdout.strip()
+        except Exception as e:
+            print("Try file but an error occurs : ", e)
+        try:
+            strings_output = subprocess.run(["strings", "-n", "5", sample_path], capture_output=True, text=True).stdout.strip()
+        except Exception as e:
+            print("Try strings but an error occurs : ", e)
+        try:
+            hexdump_head = subprocess.run(["hexdump", "-C", sample_path], capture_output=True, text=True).stdout.strip()
+            hexdump_tail = subprocess.run(["hexdump", "-C", sample_path], capture_output=True, text=True).stdout.strip()
+        except Exception as e:
+            print("Try hexdump but an error occurs : ", e)
+        md_content = generate_md_content(sample_path, md5, sha1, sha256, ssdeep_hash, imphash, clamav_result, trid_result, file_info, strings_output, hexdump_head, hexdump_tail)
         sample_name = os.path.splitext(os.path.basename(sample_path))[0] + ".md"
         output_path = os.path.join(output_folder, sample_name)
         with open(output_path, "w") as md_file:
             md_file.write(md_content)
-        print(f"Le fichier MD a été créé pour : {sample_path} -> {output_path}")
+        print(f"The MD file was created for : {sample_path} -> {output_path}")
     elif os.path.isdir(sample_path):
         for root, _, files in os.walk(sample_path):
             for file_name in files:
                 file_path = os.path.join(root, file_name)
-                analyze_sample(file_path, output_folder)
+                output_file_path = os.path.join(output_folder, os.path.splitext(file_name)[0] + ".md")
+                if not os.path.exists(output_file_path):
+                    analyze_sample(file_path, output_folder, run_clamav)
 
 def main():
-    parser = argparse.ArgumentParser(description="Script de calcul des hachages MD5, SHA-1, SHA-256, SSDEEP et IMPHASH d'un fichier ou d'un dossier.")
-    parser.add_argument("sample", help="Chemin vers un fichier ou un dossier contenant des échantillons à analyser.")
+    parser = argparse.ArgumentParser(description="Script for calculating the MD5, SHA-1, SHA-256, SSDEEP and IMPHASH hashes of a file or folder.")
+    parser.add_argument("sample", help="Path to a file or folder containing samples to analyze.")
+    parser.add_argument("--clamav", action="store_true", help="Do the freshclam and the ClamAV scan.")
     args = parser.parse_args()
 
-    # Créer le dossier sample_analysed au même niveau que le premier échantillon analysé
     first_sample_parent_folder = os.path.dirname(args.sample)
     output_folder = os.path.join(first_sample_parent_folder, "sample_analysed")
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-
-    analyze_sample(args.sample, output_folder)
+    
+    if args.clamav:
+        try:
+            subprocess.run(["sudo", "freshclam"])
+        except Exception as e:
+            print("Try freshclam but an error occurs : ", e)
+    analyze_sample(args.sample, output_folder, args.clamav)
 
 if __name__ == "__main__":
     main()
